fa5be1b7990fa0e4d2e02716c112214089795d2c
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-05-16 16:41:06 -0700

    validate field name runes

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -4,9 +4,35 @@ import (
 	"bytes"
 	"errors"
 	"strings"
+  "unicode"
+  "fmt"
 )
 
 const crlf = "\r\n"
+
+// Ensure Range Table is properly sorted
+var validHttpTokenRunes = &unicode.RangeTable{ 
+	R16: []unicode.Range16{
+	  {Lo: '!', Hi: '!', Stride: 1},
+		{Lo: '#', Hi: '#', Stride: 1},
+		{Lo: '$', Hi: '$', Stride: 1},
+		{Lo: '%', Hi: '%', Stride: 1},
+		{Lo: '&', Hi: '&', Stride: 1},
+		{Lo: '\'', Hi: '\'', Stride: 1},
+		{Lo: '*', Hi: '*', Stride: 1},
+		{Lo: '+', Hi: '+', Stride: 1},
+		{Lo: '-', Hi: '-', Stride: 1}, 
+		{Lo: '.', Hi: '.', Stride: 1},
+		{Lo: '0', Hi: '9', Stride: 1},
+		{Lo: 'A', Hi: 'Z', Stride: 1},
+		{Lo: '^', Hi: '^', Stride: 1},
+		{Lo: '_', Hi: '_', Stride: 1},
+		{Lo: '`', Hi: '`', Stride: 1},
+    {Lo: 'a', Hi: 'z', Stride: 1},
+		{Lo: '|', Hi: '|', Stride: 1},
+		{Lo: '~', Hi: '~', Stride: 1},	
+  },
+}
 
 type Headers map[string]string
 
@@ -32,11 +58,29 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
   fieldName = strings.TrimSpace(fieldName)
   fieldValue := strings.TrimSpace(string(parts[1]))
+
+  fmt.Printf("Valid Char: %v\n", isValidTChar(fieldName))
+  if !isValidTChar(fieldName) {
+    fmt.Printf("Invalid Field Name: %s\n", fieldName)
+    return 0, false, errors.New("contains invalid runes")
+  } 
+
   h.SetHeader(fieldName, fieldValue)
 
   return idx + 2, false, nil
 }
 
 func (h Headers) SetHeader(fieldName, fieldValue string) {
-  h[fieldName] = fieldValue
+  loweredName := strings.ToLower(fieldName)
+  h[loweredName] = fieldValue
+}
+
+func isValidTChar(tChar string) bool {
+  for _, c := range tChar {
+    if !unicode.Is(validHttpTokenRunes, rune(c)) {
+      fmt.Printf("%v is an invalid char\n", string(c))
+      return false
+    }
+  }
+  return true
 }
