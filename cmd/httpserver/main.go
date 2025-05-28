7345f42a662aef44483cc4f34e912c6cd7d1f211
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-05-27 18:10:52 -0700

    entry point to use server package to listen for requests

---
+++ cmd/httpserver/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"io"
+
+	"github.com/derjabineli/httpfromtcp/internal/server"
+	"github.com/derjabineli/httpfromtcp/internal/request"
+	"github.com/derjabineli/httpfromtcp/internal/response"
+
+)
+
+const port = 42069
+
+func main() {
+	server, err := server.Serve(port, handler)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+	defer server.Close()
+	log.Println("Server started on port", port)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Server gracefully stopped")
+}
+
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message: "Your problem is not my problem\n",
+		}
+	}
+	if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			Message: "Woopsie, my bad\n",
+		}
+	}
+	w.Write([]byte("All good, frfr\n"))
+	return nil
+}
+
